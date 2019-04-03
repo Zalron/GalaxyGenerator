7e2f7e4a96460ab9610530c92726b71e11375f7f
Zalron <davidrlawson@optusnet.com.au>
2019-04-03 14:37:57 +1100

    Added more code skeletons

--- GalaxyGenerator/Assets/Scripts/Sector.cs
+++ GalaxyGenerator/Assets/Scripts/Sector.cs
@@ -1,59 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public enum SectorType 
+namespace GalaxyGenerator
 {
-    Core,
-    Middle,
-    Edge,
-    Far, 
-}
-public class Sector : MonoBehaviour
-{
-
-    // Start is called before the first frame update
-    void Start()
+    public enum SectorType
     {
-        
+        Core,
+        Middle,
+        Edge,
+        Far,
     }
-
-    // Update is called once per frame
-    void Update()
+    public class Sector : MonoBehaviour
     {
 
-    }
-    public int SectorPosition;
-    public int[] SectorStars;
-    public Vector3 StarPosition;
-    public void Generate() 
-    {
-                // Fist pass, just make some random stars for us
-
-                int galaxyWidth = GalaxyConfig.GalaxyWidth;
-                for (int i = 0; i < GalaxyConfig.NumStars; i++)
-                {
-                    StarSystem ss = new StarSystem();
-                    ss.Position = new Vector3
-                        (
-                        Random.Range(-galaxyWidth / 2, galaxyWidth / 2),
-                        Random.Range(-galaxyWidth / 2, galaxyWidth / 2),
-                        Random.Range(-galaxyWidth / 2, galaxyWidth / 2)
-                        );
-                    ss.Generate(/* Do we pass exactly what time of start system we want?*/);
-                    StarSystems.Add(ss);
-                }
-                Debug.Log("Num Stars Generated:" + StarSystems.Count);
-            }
-    public void GenerateSector()
-    {
-        int SectorWidth = 100;
-        for (int i = 0; i < SectorWidth; i++) 
+        public int SectorPosition;
+        public int SectorStars;
+        public GameObject Stars;
+        public Vector3 StarPosition;
+        public Sector()
         {
-
-            Random.Range(-galaxyWidth / 2, galaxyWidth / 2),
-            Random.Range(-galaxyWidth / 2, galaxyWidth / 2),
-            Random.Range(-galaxyWidth / 2, galaxyWidth / 2)
+            SectorStars = Random.Range(1, 400);
         }
+        //public void Generate() 
+        //{
+        //    // Fist pass, just make some random stars for us
 
+        //    int galaxyWidth = GalaxyConfig.GalaxyWidth;
+        //    for (int i = 0; i < GalaxyConfig.NumStars; i++)
+        //    {
+        //        StarSystem ss = new StarSystem();
+        //        ss.Position = new Vector3
+        //            (
+        //            Random.Range(-galaxyWidth / 2, galaxyWidth / 2),
+        //            Random.Range(-galaxyWidth / 2, galaxyWidth / 2),
+        //            Random.Range(-galaxyWidth / 2, galaxyWidth / 2)
+        //            );
+        //        ss.Generate(/* Do we pass exactly what time of start system we want?*/);
+        //        StarSystems.Add(ss);
+        //    }
+        //    Debug.Log("Num Stars Generated:" + StarSystems.Count);
+        //}
+        public void GenerateSector()
+        {
+            int SectorWidth = 100;
+            for (int i = 0; i < SectorWidth; i++)
+            {
+                StarPosition = new Vector3(
+                Random.Range(0, SectorWidth),
+                Random.Range(0, SectorWidth),
+                Random.Range(0, SectorWidth));
+            }
+        }
     }
 }
