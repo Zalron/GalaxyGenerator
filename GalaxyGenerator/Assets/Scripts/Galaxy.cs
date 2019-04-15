8dad77926b0387c6f519f6e61a3fce23069f08ff
Zalron <Davidrlawson@optusnet.com.au>
2019-04-15 17:02:28 +1000

    Trying to spawn sectors around us

--- GalaxyGenerator/Assets/Scripts/Galaxy.cs
+++ GalaxyGenerator/Assets/Scripts/Galaxy.cs
@@ -1,20 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using UnityEngine;
 namespace GalaxyGenerator
 {
-    public class Galaxy
+    public class Galaxy : MonoBehaviour
     {
-        Sector sector;
+        
+        //public Sector sector;
+        public GameObject player;
+        public GameObject starSystemsObject;
+        public static int SectorSize = 100;
+        public static ConcurrentDictionary<string, Sector> sectors;
+        public static List<string> toRemove = new List<string>(); // a list to remove the chunks that are not needed from the dictionary
+        public static int galaxyHeight = 120; // the height of the world
+        public static int galaxyLength = 1000; // the height of the world
+        public static int galaxyWidth = 1000; // the height of the world
         // Start is called before the first frame update
         void Start()
         {
+            GenerateGalaxy((int)(player.transform.position.x / SectorSize), (int)(player.transform.position.y / SectorSize), (int)(player.transform.position.z / SectorSize), 2, 2);
+        }
+        public static string BuildSectorName(Vector3 v) // assigning a name to a chunk
+        {
+            return (int)v.x + "_" + (int)v.y + "_" + (int)v.z;
+        }
+        void GenerateSectorAt(int x, int y, int z)// builds chunks
+        {
+            Vector3 SectorPosition = new Vector3(x * SectorSize, y * SectorSize, z * SectorSize);
+            string sn = BuildSectorName(SectorPosition);
+            Sector _s;
+            if (!sectors.TryGetValue(sn, out _s))
+            {
+                _s = new Sector(SectorType.Core, SectorPosition, starSystemsObject, sn, this);
+                _s.starSystemsObject.transform.parent = this.transform;
+                sectors.TryAdd(_s.starSystemsObject.name, _s);
+            }
+        }
+        void GenerateGalaxy(int x, int y, int z, int startradius, int radius)// builds Sectors around the player
+        {
+            //builds chunk forward
+            GenerateSectorAt(x, y, z + 1);
+
+            //builds chunk back
+            GenerateSectorAt(x, y, z - 1);
+
+            //builds chunk left
+            GenerateSectorAt(x - 1, y, z);
+
+            //builds chunk right
+            GenerateSectorAt(x + 1, y, z - 1);
+
+            //builds chunk up
+            GenerateSectorAt(x, y + 1, z);
+
+            //builds chunk down
+            GenerateSectorAt(x, y - 1, z - 1);
 
         }
-
-        // Update is called once per frame
-        void Update()
+        public void GenerateSectorStars(Vector3 StarPosition)
         {
+            Instantiate(starSystemsObject, StarPosition, Quaternion.identity);
 
         }
     }
--- GalaxyGenerator/Assets/Scripts/Sector.cs
+++ GalaxyGenerator/Assets/Scripts/Sector.cs
@@ -10,51 +10,59 @@ namespace GalaxyGenerator
         Edge,
         Far,
     }
-    public class Sector : MonoBehaviour
+    public class Sector
     {
         public SectorType sectorType;
-        public Vector3 SectorPosition;
-        public GameObject StarSystemsObject;
-        public int SectorStars;
-        int SectorWidth = 100;
-        void Start()
+        public Sector sectorMB;
+        public Galaxy galaxy;
+        public string sectorName;
+        public Vector3 sectorPosition;
+        public GameObject starSystemsObject;
+        public int sectorStars;
+        public Sector(SectorType SectorType, Vector3 SectorPosition, GameObject StarSystemsObject, string SectorName, Galaxy Galaxy)
         {
-            SectorPosition = this.transform.position;
-            GenerateSector(sectorType);
+            sectorName = SectorName;
+            sectorType = SectorType;
+            sectorPosition = SectorPosition; 
+            starSystemsObject = StarSystemsObject;
+            galaxy = Galaxy;
+            starSystemsObject = new GameObject(Galaxy.BuildSectorName(sectorPosition));
+            starSystemsObject.transform.position = sectorPosition;
+            GenerateSector(SectorType);
         }
         public void GenerateSector(SectorType sectorType)
         {
-            Vector3 StarPosition = new Vector3(Random.Range(0, SectorWidth + 1), Random.Range(0, SectorWidth + 1), Random.Range(0, SectorWidth + 1));
+            Vector3 StarPosition = new Vector3(Random.Range(0, Galaxy.SectorSize + 1), Random.Range(0, Galaxy.SectorSize + 1), Random.Range(0, Galaxy.SectorSize + 1));
             if (sectorType == SectorType.Core)
             {
-                SectorStars = Random.Range(500, 800 + 1);
-                for (int i = 0; i < SectorStars; i++)
+                sectorStars = Random.Range(500, 800 + 1);
+                for (int i = 0; i < sectorStars; i++)
                 {
-                    Instantiate(StarSystemsObject, StarPosition, Quaternion.identity);
+                    galaxy.GenerateSectorStars(StarPosition);
                 }
             }
             else if (sectorType == SectorType.Middle)
             {
-                SectorStars = Random.Range(200, 500 + 1);
-                for (int i = 0; i < SectorStars; i++)
+                sectorStars = Random.Range(200, 500 + 1);
+                for (int i = 0; i < sectorStars; i++)
                 {
-                    Instantiate(StarSystemsObject, StarPosition, Quaternion.identity);
+                    galaxy.GenerateSectorStars(StarPosition);
                 }
             }
             else if (sectorType == SectorType.Edge)
             {
-                SectorStars = Random.Range(50, 200 + 1);
-                for (int i = 0; i < SectorStars; i++)
+                sectorStars = Random.Range(50, 200 + 1);
+                for (int i = 0; i < sectorStars; i++)
                 {
-                    Instantiate(StarSystemsObject, StarPosition, Quaternion.identity);
+                    galaxy.GenerateSectorStars(StarPosition);
                 }
             }
             else if (sectorType == SectorType.Far)
             {
-                SectorStars = Random.Range(10, 50 + 1);
-                for (int i = 0; i < SectorStars; i++)
+                sectorStars = Random.Range(10, 50 + 1);
+                for (int i = 0; i < sectorStars; i++)
                 {
-                    Instantiate(StarSystemsObject, StarPosition, Quaternion.identity);
+                    galaxy.GenerateSectorStars(StarPosition);
                 }
             }
         }
